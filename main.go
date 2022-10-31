1d107ff740062d94bb648497179da5892c05cec0
adamszpilewicz <57848391+adamszpilewicz@users.noreply.github.com>
2022-10-31 18:52:22 +0100

    add red config from viper

--- main.go
+++ main.go
@@ -6,16 +6,15 @@ import (
 	"log"
 	"techschool/api"
 	db "techschool/db/sqlc"
-)
-
-const (
-	dbDriver   = "postgres"
-	dbSource   = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddr = "0.0.0.0:8080"
+	"techschool/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot read config variables:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -23,7 +22,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddr)
+	err = server.Start(config.ServerAddr)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
